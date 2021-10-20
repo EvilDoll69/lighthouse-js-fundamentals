85f54ea713666ecaac3b5046d12d1ceba54de4bb
EvilDoll69 <tetiana.fediushyn@gmail.com>
2021-10-20 20:54:22 +0000

    Console vs return

--- hello.js
+++ hello.js
@@ -1,5 +1,22 @@
-const sayHello = function (name) {
+/*const sayHello = function (name) {
   console.log("Hello " + name);
 }
 
-sayHello("Petro");
+sayHello("Petro");*/
+
+// CONSOLE.LOG
+
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHelloToConsole("John");
+
+// RETURN
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+
+const greeting = returnSayHello("John");
+console.log(greeting);
