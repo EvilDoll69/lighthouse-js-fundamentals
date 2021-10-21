bd6f2c258d510afa8655168fbed9d836e7d65495
EvilDoll69 <tetiana.fediushyn@gmail.com>
2021-10-21 18:26:10 +0000

    How to calculate

---
+++ challenge-howManyHundreds.js
@@ -0,0 +1,12 @@
+const howManyHundreds = function(num) {
+  output = num / 100;
+  return Math.trunc(output);
+
+}
+
+console.log(howManyHundreds(80));
+
+// second option
+function howManyHundreds(num) {
+  return num % 100 === 0 ? num / 100 : (num - (num % 100)) / 100;
+}
